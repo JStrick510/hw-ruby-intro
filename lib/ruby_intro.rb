0dcd44ad4d998de65e6be3012bee12109e21ab76
JStrick510 <jacobstrickland510@gmail.com>
2022-02-09 18:07:26 +0000

    part 1 complete

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,14 +4,24 @@
 
 def sum arr
   # YOUR CODE HERE
+  arr.sum
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  if arr.length == 0
+    return 0
+  elsif arr.length == 1
+    return arr[0]
+  else
+    arr_sort = arr.sort.reverse
+    return arr_sort[0] + arr_sort[1]
+  end
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  !!arr.combination(2).detect{ |a, b| a + b == n }
 end
 
 # Part 2
